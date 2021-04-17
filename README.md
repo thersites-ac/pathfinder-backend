d305aa65b71f039c2f76ebb1e9ccf7b1b4537bae
Aaron Cogbill <aaaronc@gmail.com>
2021-04-17 19:19:42 -0400

    running locally

---
+++ README.md
@@ -0,0 +1,43 @@
+# SETUP
+
+### Dependencies
+* Docker
+* mysql-client
+* AWS SAM CLI
+* some Java JDK
+* maven
+
+### One-time setup
+Run:
+
+    docker network create pathfinder
+    docker run --name mysql --network pathfinder -d mysql/mysql-server:8.0.23
+   
+You can stop the docker container or start it up again any time with `docker stop mysql` and `docker start mysql`.
+
+### To create the database and user credentials locally
+Run:
+
+    docker logs mysql 2>/dev/null | grep GENERATED
+    
+Save the password you see. Next, run
+
+    docker exec -it mysql mysql -uroot -p
+    
+This starts a MySQL session for the containerized database. You will be prompted for a password; give the one you saved from before.
+Next, run the following MySQL commands:
+
+    > ALTER USER 'root'@'localhost' IDENTIFIED BY 'somepassword';
+    > CREATE DATABASE pathfinder;
+    > USE pathfinder;
+    > CREATE USER 'pathfinder'@'%' IDENTIFIED BY 'pathfinder';
+    > GRANT ALL PRIVILEGES ON pathfinder.* TO 'pathfinder'@'%';
+
+### To build the code
+    mvn clean package
+
+### To run locally
+    sam local start-api --docker-network pathfinder
+
+Now you can hit the endpoints on `localhost:3000` with your HTTP client of choice.
+As of this writing, the application serves `/tomblywombly` as a REST endpoint.
--- src/main/java/cogbog/App.java
+++ src/main/java/cogbog/App.java
@@ -1,15 +1,26 @@
 package cogbog;
 
+import cogbog.dao.ProfileDao;
+import cogbog.model.Profile;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.NoResultException;
 
 public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    private ProfileDao profileDao = new ProfileDao();
 
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
         logger.debug("INIT");
@@ -30,18 +41,48 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
     }
 
     private void doGet(APIGatewayProxyRequestEvent requestEvent, APIGatewayProxyResponseEvent responseEvent, Context context) {
-        String player = requestEvent.getPathParameters().get("player");
-        responseEvent.setBody(player + " HAS BIG FLOPPY FEET");
-        responseEvent.setStatusCode(200);
+        logger.info("GET");
+        int profileId = Integer.parseInt(requestEvent.getPathParameters().get("id"));
+        logger.info("Requested id: {}", profileId);
+        try {
+            Profile profile = profileDao.getProfile(profileId);
+            ObjectMapper objectMapper = new ObjectMapper();
+            requestEvent.setBody(objectMapper.writeValueAsString(profile));
+            responseEvent.setStatusCode(200);
+        } catch (JsonProcessingException e) {
+            logger.error(e.toString());
+            responseEvent.setStatusCode(500);
+        } catch (NoResultException e) {
+            logger.info(e.toString());
+            responseEvent.setBody("Not found: " + profileId);
+            responseEvent.setStatusCode(404);
+        }
     }
 
     private void doPost(APIGatewayProxyRequestEvent requestEvent, APIGatewayProxyResponseEvent responseEvent, Context context) {
+        logger.info("POST");
         String body = requestEvent.getBody();
-        responseEvent.setBody(body.toUpperCase());
-        responseEvent.setStatusCode(200);
+        logger.info("body: {}", body);
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Profile profile = objectMapper.readValue(body, Profile.class);
+            profileDao.createProfile(profile);
+            responseEvent.setBody(objectMapper.writeValueAsString(profile));
+            responseEvent.setStatusCode(201);
+        } catch (UnrecognizedPropertyException e) {
+            logger.info(e.toString());
+            responseEvent.setStatusCode(400);
+        } catch (JsonMappingException e) {
+            logger.error(e.toString());
+            responseEvent.setStatusCode(400);
+        } catch (JsonProcessingException e) {
+            logger.error(e.toString());
+            responseEvent.setStatusCode(500);
+        }
     }
 
-    private void badMethod(APIGatewayProxyRequestEvent requestEvent, APIGatewayProxyResponseEvent responseEvent, Context context) {
+        private void badMethod(APIGatewayProxyRequestEvent requestEvent, APIGatewayProxyResponseEvent responseEvent, Context context) {
+        logger.info("Unsupported method: {}", requestEvent.getHttpMethod());
         responseEvent.setStatusCode(405);
     }
 
